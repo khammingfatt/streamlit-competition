268af8cfdd5aa8fd5cf805c2bbada3ba83b131ee
Ming Fatt - Ming <130486674+khammingfatt@users.noreply.github.com>
2023-08-04 17:10:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ import streamlit as st
 
 conn = st.experimental_connection("kaggle_datasets", type=KaggleDatasetConnection)
 df = conn.get(path='katherinehudak/pokemon-gen-1-to-gen-9', filename='pokemon_data_gen1.csv', ttl=3600)
+
+st.dataframe(df)
